6dad58593c53fbe6571dba1f342d38faa4d26832
henna04 <hennafathimak1234@gmail.com>
2024-12-07 12:59:10 +0530

    stated login screen

---
+++ lib/view/login_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_image.png"),
+          ],
+        ),
+      ),
+    );
+  }
+}
