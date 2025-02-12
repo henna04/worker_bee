6e44a29b99aa0b49eaa8e73823cff8c21c0931dc
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2025-02-12 09:11:05 +0530

    Update auth_check.dart

--- lib/view/auth_check.dart
+++ lib/view/auth_check.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:worker_bee/adminView/admin_login_view.dart';
 import 'package:worker_bee/adminView/dashboard_screen.dart';
-import 'package:worker_bee/view/customNavigation/custom_navigation_view.dart';
-import 'package:worker_bee/view/login/login_view.dart';
 
 class AuthCheck extends StatelessWidget {
   const AuthCheck({super.key});
@@ -16,10 +14,10 @@ class AuthCheck extends StatelessWidget {
         if (snapshot.hasData) {
           final session = snapshot.data?.session;
           if (session != null) {
-            return const CustomNavigationView();
+            return const DashboardScreen();
           }
         }
-        return const LoginView();
+        return const AdminLoginScreen();
       },
     );
   }
